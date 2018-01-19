49f5d900b03a2ac2a191091c73ac66fc665ead6c
plum <plum@plums-MacBook-Pro.local>
2018-01-19 16:43:28 +0800

    完善header

---
+++ sell/src/components/sellers/sellers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    I am sellers
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'sellers'
+    };
+</script>
+
+<style scoped>
+
+</style>
--- sell/src/router/index.js
+++ sell/src/router/index.js
@@ -1,14 +1,33 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import sellers from 'components/sellers/sellers';
+import '../common/stylus/index.styl';
+import VueResource from 'vue-resource';
 
 Vue.use(Router);
+Vue.use(VueResource);
 
-export default new Router({
-  routes: [
-    // {
-    //   path: '/',
-    //   name: '',
-    //   component:'<a>'
-    // }
-  ]
+const routes = [
+  {
+    path: '/goods',
+    component: goods
+  },
+  {
+    path: '/ratings',
+    component: ratings
+  },
+  {
+    path: '/sellers',
+    component: sellers
+  }
+  ];
+
+const router = new Router({
+  routes,
+  linkActiveClass: 'active'
 });
+
+router.replace('/goods');
+export default router;
